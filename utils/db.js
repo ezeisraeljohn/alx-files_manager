e591e670a154c551bf29446ef31b3e00c44aecab
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-01 01:27:35 +0100

    chore: Refactor MongoDB connection initialization
    Refactor the MongoDB connection initialization in the  file to improve the code's readability and maintainability. The refactored code uses the  class from the  package to establish the connection. Error handling is included to log any connection errors to the console. This commit addresses the need for a more robust MongoDB connection initialization.

---
+++ utils/db.js
@@ -0,0 +1,43 @@
+// MongoDB connection
+
+const { MongoClient } = require('mongodb');
+class DBClient {
+  constructor () {
+    this.client = null;
+    this.db = null;
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+    this.url = `mongodb://${this.host}:${this.port}`;
+    MongoClient.connect(
+      this.url,
+      { useUnifiedTopology: true },
+      (err, client) => {
+        if (err) {
+          console.log(err);
+          this.db = null;
+        } else {
+          this.client = client;
+          this.db = client.db(this.database);
+        }
+      }
+    );
+  }
+
+  isAlive () {
+    if (this.db) return true;
+    return false;
+  }
+
+  async nbUsers () {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles () {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+
+module.exports = dbClient;
