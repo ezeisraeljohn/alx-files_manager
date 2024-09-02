8bd2c6eb4e5b197ce9de58b7a4df0de8a60aef02
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-02 11:03:55 +0100

    Changes made

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,13 +2,13 @@ import dbClient from "../utils/db";
 import redisClient from "../utils/redis";
 
 export const status = (req, res) => {
-  const db_db = dbClient.isAlive();
-  const redis_db = redisClient.isAlive();
-  res.status(200).json({ redis: redis_db, db: db_db });
+  const dbStatus = dbClient.isAlive();
+  const redisStatus = redisClient.isAlive();
+  res.status(200).json({ redis: redisStatus, db: dbStatus });
 };
 
 export const stats = async (req, res) => {
-  const users_db = await dbClient.nbUsers();
-  const files_db = await dbClient.nbFiles();
-  res.status(200).json({ users: users_db, files: files_db });
+  const numUsers = await dbClient.nbUsers();
+  const numFiles = await dbClient.nbFiles();
+  res.status(200).json({ users: numUsers, files: numFiles });
 };
