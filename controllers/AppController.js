0c6c8af4428fc9220d06a46ac7d4fb16a773dcb8
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-02 11:46:26 +0100

    Refactor AppController.js for improved readability and maintainability

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,13 +1,13 @@
 import dbClient from "../utils/db";
 import redisClient from "../utils/redis";
 
-export const status = (req, res) => {
+export const getStatus = (req, res) => {
   const dbStatus = dbClient.isAlive();
   const redisStatus = redisClient.isAlive();
   res.status(200).json({ redis: redisStatus, db: dbStatus });
 };
 
-export const stats = async (req, res) => {
+export const getStats = async (req, res) => {
   const numUsers = await dbClient.nbUsers();
   const numFiles = await dbClient.nbFiles();
   res.status(200).json({ users: numUsers, files: numFiles });
