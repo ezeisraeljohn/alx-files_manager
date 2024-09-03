f38dff8eab811c113f905122734b310732b2c0f1
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-03 04:34:29 +0100

    feat: Add route for creating new users

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import { getStatus, getStats } from "../controllers/AppController";
+import { postNew } from "../controllers/UsersController";
 const { Router } = require("express");
 
 const router = Router();
 
 router.get("/status", getStatus);
 router.get("/stats", getStats);
+router.post("/users", postNew);
 
 module.exports = router;
