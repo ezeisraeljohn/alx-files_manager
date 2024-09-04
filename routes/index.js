b6c89a6b41171f4b1afb0a26dc617834f079b156
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-04 02:26:17 +0100

    feat: Add endpoints for connecting and retrieving user information

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 import { getStatus, getStats } from "../controllers/AppController";
+import { getConnect } from "../controllers/AuthController";
 import { postNew } from "../controllers/UsersController";
+import { getMe } from "../controllers/UsersController";
 const { Router } = require("express");
 
 const router = Router();
@@ -7,5 +9,7 @@ const router = Router();
 router.get("/status", getStatus);
 router.get("/stats", getStats);
 router.post("/users", postNew);
+router.get("/connect", getConnect);
+router.get("/users/me", getMe);
 
 module.exports = router;
